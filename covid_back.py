db5486853048661cc611f97f13feb3820b7d8301
Fernando Moreira <fernando.fot84@hotmail.com>
2021-07-26 22:30:25 -0300

    adicionado os países

--- covid_back.py
+++ covid_back.py
@@ -12,8 +12,17 @@ print("Imprimindo o resultado")
 print(response_covid_summary.json())
 print("==============================")
 
+response_covid_countries = requests.get("https://api.covid19api.com/countries")
+
 def covid_print(obj):
     text1 = json.dumps(obj, sort_keys=True, indent=4)
     print(text1)
 
 covid_print((response_covid_summary.json()))
+
+
+def covid_countries(obj):
+    text2 = json.dumps(obj, sort_keys=True, indent=4)
+    print(text2)
+
+covid_countries(response_covid_countries.json())
