8851bc42f909bca666fa5feddd2073a20d924f2c
Fernando Moreira <fernando.fot84@hotmail.com>
2021-07-26 21:44:21 -0300

    Adicionado o resultado sumary puxado da API

--- covid_back.py
+++ covid_back.py
@@ -3,5 +3,11 @@ import json
 
 # Visualizar se a pagina esta funcionando
 response_covid_default = requests.get("https://api.covid19api.com/")
+print("Imprimindo o resultado")
 print(response_covid_default)
+print("==============================")
 
+response_covid_summary = requests.get("https://api.covid19api.com/summary")
+print("Imprimindo o resultado")
+print(response_covid_summary.json())
+print("==============================")
