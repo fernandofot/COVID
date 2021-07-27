122b6caf2d92de4dbf5ca0a575aa5f4196e21e1c
Fernando Moreira <fernando.fot84@hotmail.com>
2021-07-26 21:55:18 -0300

    Criando o def para organizar melhor

--- covid_back.py
+++ covid_back.py
@@ -11,3 +11,9 @@ response_covid_summary = requests.get("https://api.covid19api.com/summary")
 print("Imprimindo o resultado")
 print(response_covid_summary.json())
 print("==============================")
+
+def covid_print(obj):
+    text1 = json.dumps(obj, sort_keys=True, indent=4)
+    print(text1)
+
+covid_print((response_covid_summary.json()))
